b00b75b430fbde7821920356105b056e46452593
Oleg <gvozdoder-92@mail.ru>
2019-11-01 17:45:46 +0300

    Staged Functions and check tests for remote jobs

--- JOB_MANIPULATOR.ps1
+++ JOB_MANIPULATOR.ps1
@@ -1,57 +1,111 @@
 ﻿#chcp 65001
 #schtasks /Query /s "fobo-vrx-int2"
-Write-Host "|INT|INT|INT|INT|INT|INT|INT|INT|INT|INT|INT||INT||INT|" -BackgroundColor DarkGreen
-Write-Host "|Что будем делать повелитель?:                        |
-|1. Показать весь список задач на удаленной машине?   |
-|2. Запустим конкретный джоб?                         |
-|3. Выход из программы!                               |" -BackgroundColor Green 
-Write-Host "|INT|INT|INT|INT|INT|INT|INT|INT|INT|INT|INT||INT||INT|" -BackgroundColor DarkGreen
+$CNUM =''
+$CNAME = ''
+$Contur = ''
+$Server = ''
+$CNT_TBL = @{"1"="vrq";"2"="vrx"}
+$abs = ''
 
-$SelectJhin = Read-Host "Слушаю и повинуюсь: " 
-
-
-if ($SelectJhin -eq '1')
+Function CONTUR([string]$CNT)
 {
-  Get-ScheduledTask -CimSession $Server 
-}
-elsif ($SelectJhin -eq '2')
-{
-Write-Host "=====================
-Выберите контур:   ||
-|| 1: VRQ          ||
-|| 2: VRX          ||
-=====================" -BackgroundColor DarkYellow -ForegroundColor Black
-$Contur = Read-Host "Выберите контур"
-    if ($Contur -eq 1)
+    
+    if ($CNT -eq '1')
     {
-        $CNAME = "vrq"
-        $СNUM = Read-Host "Введите номер контура"
+        $CNAME = $CNT_TBL[$CNT]
+        $СNUM = Read-Host "Введите номер контура VRQ"
         $Server = "fobo-$CNAME-int$CNUM"
+        return $Server
         
     }
-    elseif ($Contur -eq 2)
+    elseif ($CNT -eq '2')
     {
-        $CNAME = "vrx"
-        $CNUM = Read-Host "Введите номер контура"
+        $CNAME = $CNT_TBL[$CNT]
+        $CNUM = Read-Host "Введите номер контура VRX"
         $Server = "fobo-$CNAME-int$CNUM"
+        return $Server
           
     }
 }
-$INTERFACE = "086"
 
-$PORT = Get-ScheduledTask -CimSession fobo-vrx-int2 -TaskName "FOBO*$INTERFACE*"
-$PORT.TaskName
-
-
-foreach ($p in $PORT)
+Function INTERFACES([string]$SRV)
 {
-    Write-Host "Желаете запустить задачу?" $p.taskname
-    if ($p.State -eq 'Disabled')
+    $INTERFACE = Read-host "Введите номер интерфейса в формате XXX"
+    $PORT = Get-ScheduledTask -CimSession $SRV -TaskName "FOBO*$INTERFACE*"
+    $PORT.TaskName
+    foreach ($p in $PORT)
     {
-     Write-Host "JOB В ГОВНЕ"
+        Write-Host "Желаете запустить задачу?" $p.taskname
+        if ($p.State -eq 'Disabled')
+        {
+        Write-Host "JOB В ГОВНЕ"
+        }
+        else
+        {
+        Write-host "JOB не в Говне"
+        }
     }
-    else
-    {
-    Write-host "JOB не в Говне"
-    }
+
 }
+
+
+do
+{
+    
+
+Write-Host "|IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII|" -BackgroundColor DarkGreen
+Write-Host "|ЧТО ЖЕЛАЕШЬ ПОВЕЛИТЕЛЬ?                              |
+|1. Показать весь список задач на удаленной машине?   |
+|2. Запустим конкретный джоб?                         |
+|3. Выход из программы!                               |" -BackgroundColor DarkGreen 
+Write-Host "|IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII|" -BackgroundColor DarkGreen
+
+$SelectJhin = Read-Host "|Слушаю и повинуюсь " 
+
+if ($SelectJhin -eq '0'){break}
+
+if ($SelectJhin -eq '1')
+{
+Write-Host "IIIIIIIIIIIIIIIIIIIII
+||Выберите контур: ||
+|| 1: VRQ          ||
+|| 2: VRX          ||
+IIIIIIIIIIIIIIIIIIIII" -BackgroundColor DarkGray -ForegroundColor Black
+    $Contur = Read-Host "Выберите контур"
+    CONTUR($Contur)
+    $Server
+    #Get-ScheduledTask -CimSession $Server 
+}
+elseif ($SelectJhin -eq '2')
+{
+Write-Host "IIIIIIIIIIIIIIIIIIIII
+||Выберите контур: ||
+|| 1: VRQ          ||
+|| 2: VRX          ||
+IIIIIIIIIIIIIIIIIIIII" -BackgroundColor DarkCyan -ForegroundColor Black
+
+    $Contur = Read-Host "Выберите контур"
+
+    $Connect_to_Server = CONTUR($Contur)
+    Write-Host "FUNC CONTUR FINISHED -  REULT ="$Server
+    $Result_INT = INTERFACES($Connect_to_Server)
+
+}
+
+}
+while ($SelectJhin -eq '0')
+
+
+
+#foreach ($p in $PORT)
+#{
+#    Write-Host "Желаете запустить задачу?" $p.taskname
+#    if ($p.State -eq 'Disabled')
+#    {
+#     Write-Host "JOB В ГОВНЕ"
+#    }
+#    else
+#    {
+#    Write-host "JOB не в Говне"
+#    }
+#}
