371d53ec20ece4fcbfc1871507bd71bc95ed55ce
Oleg <gvozdoder-92@mail.ru>
2019-12-09 15:16:37 +0300

    FOBO_TOOLS Version 1.8 [Fix many bugs] {Add Windows On TOP}

--- Finder.ps1
+++ Finder.ps1
@@ -1,7 +1,7 @@
-﻿#Secret Project Finder
+﻿
 
     $Icon = [system.drawing.icon]::ExtractAssociatedIcon($PSHOME + "\powershell.exe")
-    $Font = New-Object System.Drawing.Font("Colibri",9,[System.Drawing.FontStyle]::Bold)
+    $Font = New-Object System.Drawing.Font("Tempus Sans ITC",9,[System.Drawing.FontStyle]::Bold)
     $Image =  [system.drawing.image]::FromFile("\\dubovenko\D\SOFT\wallapers\Fobo_INTERFACE.jpg")
 
     #Create FOBO FORM
@@ -18,35 +18,95 @@
     $FinderForm.TopMost = $True
     $FinderForm.Icon = $Icon
 
+    #Firm Label
+    $FontBanksy = New-Object System.Drawing.Font("Tempus Sans ITC",8,[System.Drawing.FontStyle]::Bold)
+    $FOKINLAB = New-Object System.Windows.Forms.Label
+    $FOKINLAB.Location = ('290,260')
+    $FOKINLAB.Text = "Created By Fokin"
+    $FOKINLAB.Font = $FontBanksy
+    $FOKINLAB.BackColor =  'Transparent'
+    $FOKINLAB.AutoSize = $True
+
+
+    $FinderCheckGroup1 = New-Object System.Windows.Forms.GroupBox
+    $FinderCheckGroup1.Location = ('10,10')
+    $FinderCheckGroup1.Size = ('220,40')
+    $FinderCheckGroup1.BackColor = 'Transparent'
+    $FinderCheckGroup1.Text = "Formats: "
+    $FinderCheckGroup1.Font = $Font
     
+    
+    #CheckBox
+    $FinderCheckBox1 = New-Object System.Windows.Forms.CheckBox
+    $FinderCheckBox1.Location = New-Object System.Drawing.Size('10','15')
+    $FinderCheckBox1.Text = "*.xml"
+    $FinderCheckBox1.BackColor = 'Transparent'
+    $FinderCheckBox1.Name = '*.xml'
+    $FinderCheckBox1.AutoSize = $True
+    $FinderCheckBox1.Checked = $True
+    #CheckBox
+    $FinderCheckBox2 = New-Object System.Windows.Forms.CheckBox
+    $FinderCheckBox2.Location = New-Object System.Drawing.Size('65','15')
+    $FinderCheckBox2.Text ="*.txt"
+    $FinderCheckBox2.BackColor = 'Transparent'
+    $FinderCheckBox2.Name = '*.txt'
+    $FinderCheckBox2.AutoSize = $True
+    $FinderCheckBox2.Checked = $True
+    #CheckBox
+    $FinderCheckBox3 = New-Object System.Windows.Forms.CheckBox
+    $FinderCheckBox3.Location = New-Object System.Drawing.Size('115','15')
+    $FinderCheckBox3.Text ="*.html"
+    $FinderCheckBox3.BackColor = 'Transparent'
+    $FinderCheckBox3.Name = '*.html'
+    $FinderCheckBox3.AutoSize = $True
+    $FinderCheckBox3.Checked = $True
+    #CheckBOx
+    $FinderCheckBox4 = New-Object System.Windows.Forms.CheckBox
+    $FinderCheckBox4.Location = New-Object System.Drawing.Size('175','15')
+    $FinderCheckBox4.Text ="*.log"
+    $FinderCheckBox4.BackColor = 'Transparent'
+    $FinderCheckBox4.Name = '*.log'
+    $FinderCheckBox4.AutoSize = $True
+    $FinderCheckBox4.Checked = $True
 
+    $FinderSearch  = New-Object System.Windows.Forms.TextBox
+    $FinderSearch.Location = ('10,55')
+    $FinderSearch.Multiline = $true
+    $FinderSearch.Size = ('220,100')
+    $FinderSearch.BorderStyle = [System.Windows.Forms.BorderStyle]::Fixed3D
 
-    $FinderRadioFormat1 = New-Object System.Windows.Forms.CheckBox
-    $FinderRadioFormat1.Location = New-Object System.Drawing.Size('10','5')
-    $FinderRadioFormat1.Text = "*.xml"
-    $FinderRadioFormat1.BackColor = 'Transparent'
-    $FinderRadioFormat1.Name = '.xml'
-    $FinderRadioFormat1.AutoSize = $True
-    $FinderRadioFormat1.Checked = $True
+    $FinderButton = New-Object System.Windows.Forms.Button
+    $FinderButton.Location = ('240,15')
+    $FinderButton.Size = ('90,60')
+    $FinderButton.Font = $FontBanksy
+    $FinderButton.Text = "START FINDER"
 
-    $FinderRadioFormat2 = New-Object System.Windows.Forms.CheckBox
-    $FinderRadioFormat2.Location = New-Object System.Drawing.Size('60','5')
-    $FinderRadioFormat2.Text ="*.txt"
-    $FinderRadioFormat2.BackColor = 'Transparent'
-    $FinderRadioFormat2.Name = '.txt'
-    $FinderRadioFormat2.AutoSize = $True
-    $FinderRadioFormat2.Checked = $True
+    $FinderButton.add_Click({
+       $ARRAY1 = ''
+       $ARRAY1 = "{" 
 
-    $FinderRadioFormat3 = New-Object System.Windows.Forms.CheckBox
-    $FinderRadioFormat3.Location = New-Object System.Drawing.Size('105','5')
-    $FinderRadioFormat3.Text ="*.html"
-    $FinderRadioFormat3.BackColor = 'Transparent'
-    $FinderRadioFormat3.Name = '.html'
-    $FinderRadioFormat3.AutoSize = $True
-    $FinderRadioFormat3.Checked = $True
+        foreach($G in $FinderCheckGroup1.Controls)
+        {
+            
+            if($G.Checked)
+            { $ARRAY1 += $G.Name + ","}
+            else
+            {
+             
+            }
+        }
+       $Array1 = $ARRAY1.split(","[-1])
+       $ARRAY1 += "}"
+       $FinderSearch.Text = $ARRAY1
+    
+    })
 
-    $FinderForm.Controls.Add($FinderRadioFormat1)
-    $FinderForm.Controls.Add($FinderRadioFormat2)
-    $FinderForm.Controls.Add($FinderRadioFormat3)
-    #$FinderForm.Controls.AddRange(@($FinderRadioFormat1,$FinderRadioFormat2))
+    $FinderCheckGroup1.Controls.AddRange(@($FinderCheckBox1,$FinderCheckBox2,$FinderCheckBox3,$FinderCheckBox4))
+    $FinderForm.Controls.Add($FinderButton)
+    $FinderForm.Controls.Add($FinderSearch)
+    #$FinderForm.Controls.Add($FinderCheckBox1)
+    #$FinderForm.Controls.Add($FinderCheckBox2)
+    #$FinderForm.Controls.Add($FinderCheckBox3)
+    $FinderForm.Controls.Add($FinderCheckGroup1)
+    #$FinderForm.Controls.AddRange(@($FinderCheckBox1,$FinderCheckBox2))
     $FinderForm.ShowDialog()
